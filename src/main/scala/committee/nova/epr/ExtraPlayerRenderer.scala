25af2846fef5f622f5d23bc89891e7ca58feaede
MikhailTapio <mikhailtapio13468@163.com>
2022-08-18 21:35:54 +0800

    Slim

--- src/main/scala/committee/nova/epr/ExtraPlayerRenderer.scala
+++ src/main/scala/committee/nova/epr/ExtraPlayerRenderer.scala
@@ -3,7 +3,6 @@ package committee.nova.epr
 import committee.nova.epr.config.ConfigManager
 import committee.nova.epr.gui.GuiRenderConfig
 import net.minecraft.client.Minecraft
-import net.minecraft.client.settings.KeyBinding
 import net.minecraft.client.util.InputMappings
 import org.apache.logging.log4j.{LogManager, Logger}
 import org.dimdev.rift.listener.BootstrapListener
@@ -12,8 +11,6 @@ import org.lwjgl.glfw.GLFW
 
 object ExtraPlayerRenderer {
   val LOGGER: Logger = LogManager.getLogger
-  val openCfg = new KeyBinding("key.epr.opencfg.desc", InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_P,
-    "key.category.epr")
 }
 
 class ExtraPlayerRenderer extends BootstrapListener with KeybindHandler {
